82b31aea028c0123eb4c4743154596886b67a4d5
KuhnphOn <88961962+KuhnphOn@users.noreply.github.com>
2023-09-16 21:06:06 +0700

    Update views.py

--- example/views.py
+++ example/views.py
@@ -10,6 +10,7 @@ def index(request):
         <body>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
+            <h2>T4E</h2>
         </body>
     </html>
     '''
