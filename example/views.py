117890fbb0d0931043e3fcf8185cbbc74cc271eb
KuhnphOn <88961962+KuhnphOn@users.noreply.github.com>
2023-09-16 21:19:51 +0700

    Update views.py

--- example/views.py
+++ example/views.py
@@ -24,7 +24,7 @@ def index(request):
         <body>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
-            <p>The current Weather condition is { imformation.get("data")[2] }.</p>
+            <p>The current Weather condition is { information.get("data")[2] }.</p>
             <h2>T4E</h2>
         </body>
     </html>
