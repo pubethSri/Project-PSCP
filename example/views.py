654bb2d5073a91fe969212a569e0d3927c6a73fd
kxxnD04 <karnsuddee44@gmail.com>
2023-09-26 14:05:58 +0700

    ทดลองสงขอมลจากFrontมาBack

--- example/views.py
+++ example/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from . import weather_report
 
 
@@ -6,3 +7,7 @@ def index(request):
     data = weather_report.index()
     html = render(request, 'index.html', data)
     return html
+
+def request(request):
+    print(request.GET.get('test'))
+    return JsonResponse({'test_confirmed': True})
