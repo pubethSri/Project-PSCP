d781e2e75618e9b874ce3607b4ee074b4debc73f
CruzSri <138897364+pubethSri@users.noreply.github.com>
2023-11-09 21:46:37 +0700

    Delete example directory
    Try to resolve conflict
    Talk directly. Assuming that there is no threat of physical violence, talk directly to the person with whom you have the problem. ...

--- example/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from . import weather_report
-
-
-def index(request):
-    data = weather_report.index()
-    html = render(request, 'index.html', data)
-    return html
